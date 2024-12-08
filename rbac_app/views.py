1d975bcc3eb2b5bd948ebfffa1c4f0970eca106f
Raje Rj <RajeRj11@gmail.com>
2024-12-08 15:41:09 +0530

    removed unused imports

--- rbac_app/views.py
+++ rbac_app/views.py
@@ -1,8 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import User, Role, Permission, AuditLog
-from rest_framework.decorators import api_view
 from .serializers import UserSerializer, RoleSerializer, PermissionSerializer, AuditLogSerializer
 
 
